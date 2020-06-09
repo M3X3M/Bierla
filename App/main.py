10ab8e8dc451e5ad48761b5fb7019a29a4acde4f
M3X3M <mwlasak@live.com>
2020-06-09 02:22:12 +0200

    implemented barebone carousel

--- App/main.py
+++ App/main.py
@@ -4,6 +4,7 @@ from kivy.app import App
 from kivy.uix.label import Label
 from kivy.uix.widget import Widget
 from kivy.utils import rgba
+from kivy.uix.carousel import Carousel
 
 # very important see documentation
 kivy.require("1.11.1")
