9311c232b2bf379e1592817369297e002e41c26e
M3X3M <mwlasak@live.com>
2020-07-14 16:03:40 +0200

    added memberscarousel

--- App/main.py
+++ App/main.py
@@ -5,6 +5,9 @@ from kivy.uix.label import Label
 from kivy.uix.widget import Widget
 from kivy.utils import rgba
 from kivy.uix.carousel import Carousel
+from kivy.uix.boxlayout import BoxLayout
+
+from member import Member
 
 # very important see documentation
 kivy.require("1.11.1")
@@ -16,9 +19,43 @@ class BierlaApp(App):
 
     def build(self):
         self.designElements = DesignElements()
-        self.mainCarousel = self.designElements.ids['crsMain']
+        self.main_carousel = self.designElements.ids['crsMain']
+        self.members_carousel = self.designElements.ids['crsMembers']
+        self.members = []
+        self.fillMembersArray()
+        self.buildMembersCarousel()
 
         return self.designElements
+
+    #filling the members array with the members. Will be used to read
+    #json file later     
+    def fillMembersArray(self):
+        max = Member(["max","nax","wlasax"],[],["ja ich wars"])
+        oli = Member(["schmolli","wolli","trolli"],[],["lel"])
+        simon = Member(["simon","riemon","ziehmon"],[],["ich weiß"])
+
+        self.members.append(max)
+        self.members.append(oli)
+        self.members.append(simon)
+
+    #buildes the different pages of the members carousel
+    def buildMembersCarousel(self):
+        for member in self.members:
+            #creating the layout that holds all the fields
+            layout = BoxLayout(orientation='vertical')
+
+            child1_layout_names = BoxLayout(orientation='horizontal')
+            child1_firstname = Label(text=member.getName(1))
+            child1_middlename = Label(text=member.getName(2))
+            child1_lastname = Label(text=member.getName(3))
+            child1_layout_names.add_widget(child1_firstname)
+            child1_layout_names.add_widget(child1_middlename)
+            child1_layout_names.add_widget(child1_lastname)
+            
+            layout.add_widget(child1_layout_names)
+
+            self.members_carousel.add_widget(layout)
+
 
 
 if __name__ == '__main__':
--- App/member.py
+++ App/member.py
@@ -1,5 +1,5 @@
 class Member:
-    def __init__(self, names, pictures, sayings):
+    def __init__(self, names, pictures, statements):
         #initialising the names of the member, they need to be provided
         try:
             self.first_name = names[0]
@@ -12,4 +12,28 @@ class Member:
         #initialising the location where the pictures are stored
         self.pictures = []
         for picture in pictures:
-            self.pictures.append(picture)    
+            self.pictures.append(picture)
+
+        #initialising the statements of the member
+        self.statements = []
+        for statement in statements:
+            self.statements.append(statement)
+
+    #for getting a specific name. 1 = first-, 2 = middle-, 3 = last-name
+    def getName(self, nameType):
+        if nameType == 1:
+            return self.first_name
+        elif nameType == 2:
+            return self.middle_name
+        elif nameType == 3:
+            return self.last_name
+
+    def getStatements(self):
+        return self.statements
+
+    def getPictures(self):
+        return self.pictures
+
+#max = Member(["max","lol","sack"], [], ["halo"])
+#print(max.getName(2))
+
