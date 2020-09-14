6600430b55a3744052b859100e79ce7cfb717ba1
M3X3M <mwlasak@live.com>
2020-09-14 23:49:17 +0200

    updated members visuals

--- App/widget_builder.py
+++ App/widget_builder.py
@@ -98,7 +98,7 @@ def buildRuleView(rule, *args):
     main_layout.add_widget(header)
     main_layout.add_widget(body)
 
-    rule_view = ModalView(size_hint = (.9,.95), 
+    rule_view = ModalView(size_hint = (.92,.98), 
         background = "Resources/262626.png", background_color=rgba('#0a5e00'))
     rule_view.add_widget(main_layout)
     rule_view.open()
@@ -124,39 +124,42 @@ def buildMembersCarousel(carousel, members, instance):
                 #creating the layout that holds all the fields
                 layout = FloatLayout()
 
+                #with the bar on the side the new center is .425 which should 
+                #mean we need to set everything .075 to the left 
+
                 person_image = Image(source=member.getNextPicture(), 
                     allow_stretch=True, 
-                    pos_hint={'x':.2, 'top':.99}, size_hint=(.6,.3))
+                    pos_hint={'x':.15, 'top':.99}, size_hint=(.6,.3))
 
                 btn_next_picture = Button(background_normal='', 
                     background_color=rgba(0,0,0,0), 
-                    pos_hint={'x':.2, 'top':.99}, size_hint=(.6,.3))
+                    pos_hint={'x':.15, 'top':.99}, size_hint=(.6,.3))
 
                 btn_next_picture.bind(
                     on_press=partial(instance.nextPictureCallback, 
                     loop_counter, person_image))
 
                 layout_names = BoxLayout(orientation='horizontal', 
-                    pos_hint={'x':.1, 'y':.6}, size_hint=(.8,.1))
+                    pos_hint={'x':.05, 'y':.6}, size_hint=(.8,.1))
                 lbl_names = Label(text=member.getName(1) + " " + '[i]' + 
                     member.getName(2) + '[/i]' + " " + member.getName(3), 
                     markup=True, font_size='20sp')
                 layout_names.add_widget(lbl_names)
 
                 back_label = CustomRectangle(0,0,0,1, 
-                    pos_hint={'x':0, 'y':.2}, size_hint=(1,.4))
+                    pos_hint={'x':.05, 'y':0}, size_hint=(.8,.68))
 
                 btn_statement = Button(text=member.getNextStatement(), 
                     font_size='15sp', background_normal='', 
                     background_color=rgba(0,0,0,0),
-                    pos_hint={'x':.1, 'y':.2}, size_hint=(.8,.4))
+                    pos_hint={'x':.05, 'y':.2}, size_hint=(.8,.4))
 
                 btn_statement.bind(
                     on_press=partial(instance.nextStatementCallback, 
                     loop_counter, btn_statement))
 
                 lbl_birthday = Label(text=member.getBirthday(), 
-                    font_size='15sp', pos_hint={'x':.2, 'bottom':1}, 
+                    font_size='15sp', pos_hint={'x':.15, 'bottom':1}, 
                     size_hint=(.6,.1))
 
                 #adding a vertical row of buttons to quickly navigate to 
